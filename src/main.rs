f096d205d9e42e6152da70c6244d08cbdaed513b
grantkee <grant@ether.ai>
2022-04-22 17:29:46 -0600

    inital structs - async main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
+use serde::{Deserialize, Serialize};
+type Library = Vec<Book>;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Book {
+    id: usize,
+    title: String,
+    author: String,
+    publisher: String,
+    public: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum ListMode {
+    ALL,
+    One(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListRequest {
+    mode: ListMode,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListResponse {
+    mode: ListMode,
+    data: Library,
+    receiver: String,
+}
+
+enum EventType {
+    Response(ListResponse),
+    Input(String),
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
