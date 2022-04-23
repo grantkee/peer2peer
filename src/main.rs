869049f6785e7e14575dd9388daebf81205d169d
grantkee <grant@ether.ai>
2022-04-23 14:21:06 -0600

    tokio mpsc channel and auth keys

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::convert::identity;
-
+use tokio::{sync::mpsc};
 use serde::{Deserialize, Serialize};
 use log::{info, error};
 use libp2p::{
     floodsub::{Floodsub, Topic},
     identity,
+    noise::{Keypair, X25519Spec},
     NetworkBehaviour, PeerId, Transport,
 };
 use once_cell::sync::Lazy;
+
 
 const STORAGE_PATH: &str = "./library.json";
 type Library = Vec<Book>;
@@ -55,4 +56,11 @@ enum EventType {
 async fn main() {
     pretty_env_logger::init();
     info!("Peer Id: {}", PEER_ID.clone());
+
+    // multi-producer, single-consumer queue for sending values across asynchronous tasks.
+    // aka - async channel for communicating between different parts of the application
+    let (response_sender, mut response_receiver) = mpsc::unbounded_channel();
+
+    // authentication keys using noise protocol
+    let auth_keys = Keypair::<X25519Spec>::new().into_authentic(&KEYS).expect("unable to create auth keys");
 }
